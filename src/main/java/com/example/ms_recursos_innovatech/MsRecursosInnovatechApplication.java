44d59a7b681bb4298f5c6c2f8df9604de4510432
Eduardo Urbina <edu.urbina@duocuc.cl>
2026-05-03 20:27:51 -0400

    Primer commit microservicio colaboracion

---
+++ src/main/java/com/example/ms_recursos_innovatech/MsRecursosInnovatechApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_recursos_innovatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRecursosInnovatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRecursosInnovatechApplication.class, args);
+	}
+
+}
